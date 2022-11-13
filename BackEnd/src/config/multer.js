2c408217604d7490eef52d57794c54ccc3fceac3
Itatiano <itaticontato@gmail.com>
2022-11-13 12:46:47 -0300

    🐛 Fixed cryptography

--- BackEnd/src/config/multer.js
+++ BackEnd/src/config/multer.js
@@ -5,16 +5,16 @@ const crypto = require("crypto");
 module.exports = {
     dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
     storage: multer.diskStorage({
-        destination: (req, res, cb) => {
+        destination: (req, file, cb) => {
             cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
         },
-        // filename: (req, res, cb) => {
-        //     crypto.randomBytes(16, (err, hash) =>{
-        //         if (err) cb(err);
-        //         const fileName = `${hash.toString('hex')}-${file.originalname}`;
-        //         cb(null,fileName);
-        //     });
-        // },
+       filename: (req, file, cb) =>{
+        crypto.randomBytes(16, (err, hash) =>{
+            if(err) cb(err);
+            const fileName = `${hash.toString("hex")}-${file.originalname}`;
+            cb(null, fileName);
+        });
+       },
     }),
     limits: {
         fileSize: 2 * 1024 * 1024,
