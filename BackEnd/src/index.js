a3084fda83475c6ec0594f7e745db41a837175ea
Itatiano <itaticontato@gmail.com>
2022-11-13 17:54:20 -0300

    ✨ Connection with mongoDB

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,6 +1,18 @@
+// use our mongoDb conection link
+const key = require("./key/key")
+
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+
 const app = express();
+
+mongoose.connect(
+    key,
+    {
+        useNewUrlParser: true
+    }
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
