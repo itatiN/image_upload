e81bdb0a1ea4a6901524c13e9ffc186dbc17fb97
Itatiano <itaticontato@gmail.com>
2022-11-17 06:37:36 -0300

    ✨ URL

--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -5,7 +5,7 @@ const Post = require("./models/Post");
 
 routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
 
-    const{originalname: name, size, key, url = ''} = req.file;
+    const{originalname: name, size, key, location: url = ''} = req.file;
 
     const post = await Post.create({
         name,
